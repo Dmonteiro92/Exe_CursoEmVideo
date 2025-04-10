e6ee7cf8783395435aef5d6373677f2b6c048116
Dmonteiro92 <61996059+Dmonteiro92@users.noreply.github.com>
2025-04-09 22:13:19 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,12 @@
+#Leia um número e mostre seu sucessor e antecessor
+
+
+print('===== Desafio 05 - Sucessor e Antecessor =====')
+
+n1 = int(input('Digite um número inteiro:'))
+n2 = n1+1
+n3 = n1-1
+
+print('Você digitou o valor: {}'.format(n1))
+print('O sucessor do seu número é: {}'.format(n2))
+print('O antecessor do seu número é: {}'.format(n3))
